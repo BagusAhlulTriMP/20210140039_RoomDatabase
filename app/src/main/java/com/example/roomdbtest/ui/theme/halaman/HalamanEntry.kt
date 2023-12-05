9a17d526db548caa5719fe692a5a305123aed633
Bagus Ahlul <bagus.ahlul.ft21@mail.umy.ac.id>
2023-12-06 06:53:35 +0700

    menambahkan navigasi

---
+++ app/src/main/java/com/example/roomdbtest/ui/theme/halaman/HalamanEntry.kt
@@ -0,0 +1,10 @@
+package com.example.roomdbtest.ui.theme.halaman
+
+import com.example.roomdbtest.R
+import com.example.roomdbtest.navigasi.DestinasiNavigasi
+
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
