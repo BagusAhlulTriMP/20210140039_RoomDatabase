72d0f3d633a98c580849e7d6577f33d4a5cc2229
Bagus Ahlul <bagus.ahlul.ft21@mail.umy.ac.id>
2023-12-05 17:03:17 +0700

    add class homeuistate

--- app/src/main/java/com/example/roomdbtest/model/HomeViewModel.kt
+++ app/src/main/java/com/example/roomdbtest/model/HomeViewModel.kt
@@ -1,4 +1,17 @@
 package com.example.roomdbtest.model
 
-class HomeViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.roomdbtest.data.Siswa
+import com.example.roomdbtest.repositori.RepositoriSiswa
+
+class HomeViewModel(private val repositoriSiswa: RepositoriSiswa):ViewModel() {
+
+    companion object{
+        private const val TIMEOUT_MILLIS =5_000L
+    }
+
+
+    data class HomeUiState(
+        val listSiswa: List<Siswa> = listOf()
+    )
 }
