246ddb6ef34abe3174e7a71abf4ab0d765266995
Bagus Ahlul <bagus.ahlul.ft21@mail.umy.ac.id>
2023-12-06 09:10:38 +0700

    menambahkan objek DestinasiHome

--- app/src/main/java/com/example/roomdbtest/ui/theme/halaman/HalamanHome.kt
+++ app/src/main/java/com/example/roomdbtest/ui/theme/halaman/HalamanHome.kt
@@ -1,2 +1,10 @@
 package com.example.roomdbtest.ui.theme.halaman
 
+import com.example.roomdbtest.R
+import com.example.roomdbtest.navigasi.DestinasiNavigasi
+
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
