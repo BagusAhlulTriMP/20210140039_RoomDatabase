7890ecc97ef26f12edf0341842a8724ef89af258
Bagus Ahlul <bagus.ahlul.ft21@mail.umy.ac.id>
2023-12-06 07:49:45 +0700

    menambahkan fun FormInputSiswa

--- app/src/main/java/com/example/roomdbtest/ui/theme/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/roomdbtest/ui/theme/halaman/HalamanEntry.kt
@@ -1,11 +1,20 @@
 package com.example.roomdbtest.ui.theme.halaman
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.roomdbtest.R
 import com.example.roomdbtest.model.Detailsiswa
@@ -13,6 +22,7 @@ import com.example.roomdbtest.model.EntryViewModel
 import com.example.roomdbtest.model.PenyediaViewModel
 import com.example.roomdbtest.model.UIStateSiswa
 import com.example.roomdbtest.navigasi.DestinasiNavigasi
+import kotlinx.coroutines.launch
 
 
 object DestinasiEntry: DestinasiNavigasi {
@@ -30,8 +40,28 @@ fun EntrySiswaScreen(
     val coroutineScope = rememberCoroutineScope()
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
     Scaffold (
-        modifier = modifier
-    )
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiEntry.titleRes),
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior
+            )
+        }) { innerPadding ->
+        EntrySiswaBody(uiStateSiswa = viewModel.uiStateSiswa,
+            onSiswaValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                viewModel.saveSiswa()
+                navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+            )
+    }
 }
 
 @Composable
@@ -40,4 +70,26 @@ fun EntrySiswaBody(
     onSiswaValueChange: (Detailsiswa) -> Unit,
     onSaveClick: () -> Unit,
     modifier: Modifier =Modifier
-) {}
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_large)),
+        modifier = modifier.padding(dimensionResource(id = R.dimen.padding_medium))
+    ) {}
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInputSiswa(
+    detailsiswa: Detailsiswa,
+    modifier: Modifier = Modifier,
+    onValueChange: (Detailsiswa) -> Unit = {},
+    enabled: Boolean = true
+){
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+    ){
+
+    }
+}
