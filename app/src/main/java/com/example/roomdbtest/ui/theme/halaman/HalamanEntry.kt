00054727e0fc9d7f1fd1d413b83751c60b06f523
Bagus Ahlul <bagus.ahlul.ft21@mail.umy.ac.id>
2023-12-06 07:29:03 +0700

    menambahkan fun EntrySiswaBody

--- app/src/main/java/com/example/roomdbtest/ui/theme/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/roomdbtest/ui/theme/halaman/HalamanEntry.kt
@@ -1,12 +1,17 @@
 package com.example.roomdbtest.ui.theme.halaman
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.roomdbtest.R
+import com.example.roomdbtest.model.Detailsiswa
 import com.example.roomdbtest.model.EntryViewModel
 import com.example.roomdbtest.model.PenyediaViewModel
+import com.example.roomdbtest.model.UIStateSiswa
 import com.example.roomdbtest.navigasi.DestinasiNavigasi
 
 
@@ -21,4 +26,18 @@ fun EntrySiswaScreen(
     navigateBack: () -> Unit,
     modifier: Modifier = Modifier,
     viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold (
+        modifier = modifier
+    )
+}
+
+@Composable
+fun EntrySiswaBody(
+    uiStateSiswa: UIStateSiswa,
+    onSiswaValueChange: (Detailsiswa) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier =Modifier
 ) {}
