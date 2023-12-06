6620a4bf4137b11a3ee1ea95e6d74020c77c802f
Bagus Ahlul <bagus.ahlul.ft21@mail.umy.ac.id>
2023-12-06 07:54:25 +0700

    menambahkan detail nama siswa

--- app/src/main/java/com/example/roomdbtest/ui/theme/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/roomdbtest/ui/theme/halaman/HalamanEntry.kt
@@ -7,7 +7,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
@@ -90,6 +92,13 @@ fun FormInputSiswa(
         modifier = modifier,
         verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
     ){
-
+        OutlinedTextField(
+            value = detailsiswa.nama,
+            onValueChange = {onValueChange(detailsiswa.copy(nama = it))},
+            label = { Text(stringResource(R.string.nama)) },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
     }
 }
