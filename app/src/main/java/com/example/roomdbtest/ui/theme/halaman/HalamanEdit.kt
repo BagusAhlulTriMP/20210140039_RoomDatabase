cd5a03c13c6405b2ae062b721beb954926d28cbb
Bagus Ahlul <bagus.ahlul.ft21@mail.umy.ac.id>
2023-12-14 20:22:45 +0700

    menambahkan object itemdestination

--- app/src/main/java/com/example/roomdbtest/ui/theme/halaman/HalamanEdit.kt
+++ app/src/main/java/com/example/roomdbtest/ui/theme/halaman/HalamanEdit.kt
@@ -1,2 +1,12 @@
 package com.example.roomdbtest.ui.theme.halaman
 
+import com.example.roomdbtest.R
+import com.example.roomdbtest.navigasi.DestinasiNavigasi
+
+
+object ItemEditDestination : DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+    const val itemIdArg = "itemId"
+    val routeWithArgs = "$route/{$itemIdArg"
+}
