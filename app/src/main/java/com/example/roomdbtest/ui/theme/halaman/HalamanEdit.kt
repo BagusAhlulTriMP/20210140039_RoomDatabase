08659e28ef4a4a39f0b0ee22a0c2f16388c2aa04
Bagus Ahlul <bagus.ahlul.ft21@mail.umy.ac.id>
2023-12-15 09:16:00 +0700

    menambahkan val coroutinescope

--- app/src/main/java/com/example/roomdbtest/ui/theme/halaman/HalamanEdit.kt
+++ app/src/main/java/com/example/roomdbtest/ui/theme/halaman/HalamanEdit.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -12,6 +13,7 @@ import com.example.roomdbtest.model.EditViewModel
 import com.example.roomdbtest.model.PenyediaViewModel
 import com.example.roomdbtest.navigasi.DestinasiNavigasi
 import com.example.roomdbtest.navigasi.SiswaTopAppBar
+import kotlinx.coroutines.launch
 
 
 object ItemEditDestination : DestinasiNavigasi {
@@ -29,6 +31,7 @@ fun ItemEditScreen(
     modifier: Modifier = Modifier,
     viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ) {
+    val coroutineScope = rememberCoroutineScope()
     Scaffold(
         topBar = {
             SiswaTopAppBar(
